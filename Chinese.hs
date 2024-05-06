8a32e797503fa1d9beb354820cece8ade893086f
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2024-05-05 21:32:59 -0300

    chore: nesting definitions

--- Chinese.hs
+++ Chinese.hs
@@ -6,38 +6,35 @@ import Data.Char (ord, chr)
 toPairedChars :: String -> [Char]
 toPairedChars str =
     map chr $ strToPairedOctets str
-
-strToPairedOctets :: String -> [Int]
-strToPairedOctets str =
-    groupOctets $ map ord str
-
-groupOctets :: [Int] -> [Int]
-groupOctets [] = []
-groupOctets [x] = [shiftL8 x]
-groupOctets (x:y:xs) = groupOctets' x y : groupOctets xs
-
-groupOctets' :: Int -> Int -> Int
-groupOctets' left right = (shiftL8 left) .|. right
-
-shiftL8 :: Int -> Int
-shiftL8 val = val `shiftL` 8
-
--- 
+    where
+      strToPairedOctets :: String -> [Int]
+      strToPairedOctets str =
+          groupOctets $ map ord str
+          where
+            groupOctets :: [Int] -> [Int]
+            groupOctets [] = []
+            groupOctets [x] = [x `shiftL` 8]
+            groupOctets (x:y:xs) =
+              groupOctets' x y : groupOctets xs
+              where
+                groupOctets' :: Int -> Int -> Int
+                groupOctets' left right = (left `shiftL` 8) .|. right
 
 fromPairedChars :: [Char] -> String
 fromPairedChars chars =
-   pairsToString $ expandPairedOctet $ map ord chars 
-
-expandPairedOctet :: [Int] -> [[Int]]
-expandPairedOctet [] = []
-expandPairedOctet (x:xs) = expandPairedOctet' x : expandPairedOctet xs
-
-expandPairedOctet' :: Int -> [Int]
-expandPairedOctet' x 
-    | lastPair == 0 = [x `shiftR` 8 .&. 0xFF]
-    | otherwise = [x `shiftR` 8 .&. 0xFF, lastPair]
-    where lastPair = x .&. 0xFF
-
-pairsToString :: [[Int]] -> String
-pairsToString pairs =
-    map chr $ concat pairs
+  pairsToString $ expandPairedOctet $ map ord chars 
+  where
+    pairsToString :: [[Int]] -> String
+    pairsToString pairs =
+      map chr $ concat pairs
+    expandPairedOctet :: [Int] -> [[Int]]
+    expandPairedOctet [] = []
+    expandPairedOctet (x:xs) =
+      expandPairedOctet' x : expandPairedOctet xs
+      where
+        expandPairedOctet' :: Int -> [Int]
+        expandPairedOctet' x 
+          | lastPair == 0 = [x `shiftR` 8 .&. 0xFF]
+          | otherwise = [x `shiftR` 8 .&. 0xFF, lastPair]
+          where
+            lastPair = x .&. 0xFF
